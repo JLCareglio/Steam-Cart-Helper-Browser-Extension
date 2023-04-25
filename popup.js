924d990b663476e9763238c08203fcfa85fba6f4
Jesús Lautaro Careglio Albornoz <j.l.careglio.albornoz@gmail.com>
2023-04-25 15:33:39 -0300

    🚧 Codigo de ejemplo

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
 console.log("Click test - SCH");
+// Ejemplo para obtener lista de amigos de 76561199236658330 
+// const resp = await fetch(
+//   "https://steamcommunity.com/profiles/76561199236658330/friends/"
+// );
+// const html = await resp.text();
+// const parser = new DOMParser();
+// const doc = parser.parseFromString(html, "text/html");
+// doc.querySelectorAll(".selectable.friend_block_v2.persona");
