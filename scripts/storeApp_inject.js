060aae042a980ec435c5cc571a9557a944ceb0aa
JLCareglio <j.l.careglio.albornoz@gmail.com>
2023-05-26 09:23:04 -0300

    💄 Ubicacion de btns mas intuitiva + SteamDBLinks

--- scripts/storeApp_inject.js
+++ scripts/storeApp_inject.js
@@ -12,7 +12,7 @@
 
   [...subIds, ...bundleIds].forEach((purchase) => {
     const purchaseId = purchase.value;
-    if (purchase.value) {
+    if (purchaseId) {
       const typeId = purchase.name;
       const gameE = purchase.closest(".game_area_purchase_game_wrapper");
       const name = gameE
@@ -20,6 +20,9 @@
         .textContent.trim()
         .replace(/^(\w+\s)/, "")
         .split("\t")[0];
+      const btn_addtocart = gameE.querySelector(
+        ".btn_addtocart > .btn_green_steamui"
+      ).parentNode;
 
       const button = document.createElement("button");
       const hasPurchaseInLists = savedPurchaseIdLists.some((list) =>
@@ -28,11 +31,9 @@
       button.innerHTML = hasPurchaseInLists
         ? "🗑️ eliminar" //"📝 Administrar"
         : "💾 guardar";
-      button.classList.add("btn_black");
-      button.style.height = "22px";
-      button.style.width = "104px";
-      button.style.padding = "0 4px";
-      purchase.parentElement.after(button);
+      button.classList.value = "btn_save";
+      button.style.width = btn_addtocart.clientWidth + "px";
+      btn_addtocart.appendChild(button);
 
       button.addEventListener("click", async () => {
         savedPurchaseIdLists = (
@@ -73,6 +74,44 @@
         }
         chrome.storage.local.set({ savedPurchaseIdLists });
       });
+
+      const aux = gameE.children[1];
+      if (
+        aux &&
+        !aux.classList.contains("ds_flag") &&
+        !aux.classList.contains("game_area_purchase_not_refundable")
+      ) {
+        if (aux.classList.contains("game_purchase_area_friends_want"))
+          aux.querySelector("p:nth-child(2)").style.maxWidth =
+            btn_addtocart.offsetLeft -
+            parseInt(
+              window.getComputedStyle(aux).paddingLeft.replace("px", "")
+            ) +
+            "px";
+        else {
+          let paddingTop = parseInt(
+            window.getComputedStyle(aux).paddingTop.replace("px", "")
+          );
+          aux.style.paddingTop = paddingTop + 25 + "px";
+        }
+      }
+
+      // Agregando enlaces a SteamSB
+      let steamdb = document.createElement("a");
+      if (typeId == "subid") {
+        steamdb.classList.value = "btn_black btn_small btn_steamDB_subid";
+        steamdb.href = `https://steamdb.info/sub/${purchaseId}/`;
+        steamdb.innerHTML = `<span data-tooltip-text="View on SteamDB">Sub ${purchaseId}</span>`;
+        gameE.querySelector(".game_purchase_action").prepend(steamdb);
+      } else {
+        const packageE = gameE.querySelector(".btn_addtocart.btn_packageinfo");
+        steamdb.classList.value = "btn_black btn_small btn_steamDB_bundleid";
+        steamdb.href = `https://steamdb.info/bundle/${purchaseId}/`;
+        steamdb.innerHTML = `<span data-tooltip-text="View on SteamDB">Bundle ${purchaseId}</span>`;
+        steamdb.style.width = packageE.clientWidth + "px";
+        packageE.appendChild(steamdb);
+        packageE.parentNode.style.float = "left"
+      }
     }
   });
 })();
